6665a05cb82519da02595dad40bd9943232ef5b4
Jonathan Tavares Ascencio <jony16344@gmail.com>
2024-08-02 13:54:22 -0600

    venta

--- src/app/services/venta/venta.service.ts
+++ src/app/services/venta/venta.service.ts
@@ -4,6 +4,7 @@ import { param } from 'jquery';
 import { Observable, catchError } from 'rxjs';
 import { intProductoVenta, intVenta } from './ventaInterface';
 import { handleError } from '../functions';
+import { URL } from '../../../assets/const';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class VentaService {
   constructor(private http: HttpClient) { }
 
   agregarVenta(usuario: number, cliente: number, total: number, estado: number, isEfectivo: number):Observable<intVenta>{
-    return this.http.post<intVenta>('http://localhost:8080/api/ventas/agregar', null, {params:{
+    return this.http.post<intVenta>(`${URL}/api/ventas/agregar`, null, {params:{
       'usuario': usuario,
       'cliente': cliente,
       'total': total,
@@ -23,7 +24,7 @@ export class VentaService {
   }
 
   agregarVentaProducto(venta: number, producto: number | undefined, cantidad: number, subTotal: number, precio: number, nombre: string):Observable<intProductoVenta>{
-    return this.http.post<intProductoVenta>('http://localhost:8080/api/ventas/agregar-vp', null, {params:{
+    return this.http.post<intProductoVenta>(`${URL}/api/ventas/agregar-vp`, null, {params:{
       'venta': venta,
       'producto': producto?producto:1,
       'cantidad': cantidad,
@@ -34,39 +35,39 @@ export class VentaService {
   }
 
   mostrarVentas():Observable<Array<intVenta>>{
-    return this.http.get<Array<intVenta>>('http://localhost:8080/api/ventas/mostrar').pipe(catchError(handleError))
+    return this.http.get<Array<intVenta>>(`${URL}/api/ventas/mostrar`).pipe(catchError(handleError))
   }
 
   mostrarVenta(id: number):Observable<intVenta>{
-    return this.http.get<intVenta>(`http://localhost:8080/api/ventas/get-venta/${id}`).pipe(catchError(handleError))
+    return this.http.get<intVenta>(`${URL}/api/ventas/get-venta/${id}`).pipe(catchError(handleError))
   }
 
   mostrarVentaProducto(id: number):Observable<Array<intProductoVenta>>{
-    return this.http.get<Array<intProductoVenta>>(`http://localhost:8080/api/ventas/mostrar-vp/${id}`).pipe(catchError(handleError))
+    return this.http.get<Array<intProductoVenta>>(`${URL}/api/ventas/mostrar-vp/${id}`).pipe(catchError(handleError))
   }
 
   mostrarVentasPorCobrar():Observable<Array<any>>{
-    return this.http.get<Array<any>>('http://localhost:8080/api/ventas/por-pagar').pipe(catchError(handleError))
+    return this.http.get<Array<any>>(`${URL}/api/ventas/por-pagar`).pipe(catchError(handleError))
   }
 
   pagarVenta(id: number, tipo: number, isEfectivo: number):Observable<any>{
     if (tipo === 1) {
-      return this.http.get<any>(`http://localhost:8080/api/ventas/pagar/${id}`, {params:{
+      return this.http.get<any>(`${URL}/api/ventas/pagar/${id}`, {params:{
         isEfectivo: isEfectivo
       }}).pipe(catchError(handleError))
     }else{
-      return this.http.get<any>(`http://localhost:8080/api/nota-entrega/pagar/${id}`, {params:{
+      return this.http.get<any>(`${URL}/api/nota-entrega/pagar/${id}`, {params:{
         isEfectivo: isEfectivo
       }}).pipe(catchError(handleError))
     }
   }
 
   cancelarVenta(id: number):Observable<any>{
-    return this.http.post<any>(`http://localhost:8080/api/ventas/cancelar/${id}`, null).pipe(catchError(handleError))
+    return this.http.post<any>(`${URL}/api/ventas/cancelar/${id}`, null).pipe(catchError(handleError))
   }
 
   filtrarVentas(search: string):Observable<Array<intVenta>>{
-    return this.http.get<Array<intVenta>>(`http://localhost:8080/api/ventas/filtro`, {params:{
+    return this.http.get<Array<intVenta>>(`${URL}/api/ventas/filtro`, {params:{
       'search': search
     }}).pipe(catchError(handleError))
   }
